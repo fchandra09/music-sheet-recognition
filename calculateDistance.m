b54545846eb957848ae3d440ea5daaa77a6e4c89
hzhang40 <zzh915@gmail.com>
2016-05-01 01:26:45 -0500

    ttlt worked

--- calculateDistance.m
+++ calculateDistance.m
@@ -1,48 +1,50 @@
-function [output_args] = calculateDistance(img)
+function [match, graph] = calculateDistance(img, level)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+if strcmp('simple', level)
+imagefiles = dir('train_simple/*.jpeg');  
+folder = 'train_simple/';     
+else
 imagefiles = dir('training_data/*.jpeg');  
-folder = 'training_data'; 
+folder = 'training_data/'; 
+end
+
 nfiles = length(imagefiles); 
-disp(nfiles); 
 scores = zeros(nfiles, 1); 
-
+[img_h, img_w] = size(img);
+%image = im2double(rgb2gray(imread(img)));
 for i = 1:nfiles
-    filename = imagefiles(i).name; 
-    %disp(filename); 
-    target_img = imread(strcat(folder,'/', filename)); 
-    %disp(target_img); 
-    %[height, width] = size(target_img); 
-
-    %target_img = rgb2gray(target_img);
-    %disp(target_img);     
-	target_img = im2double(target_img);
-    %disp(target_img); 
-    
-    %input_resize = rgb2gray(img); 
-    %input_resize = im2double(input_resize);
-    input_resize = imread(img); 
-    %disp(input_resize); 
-    input_resize = im2double(input_resize);
-    %disp(input_resize); 
+	target_img = im2double(rgb2gray(imread([folder, num2str(i), '.jpeg'])));
+    target_img (target_img < 0.9) = 0;
+    target_img (target_img >= 0.9) = 1;
     [h, w] = size(target_img); 
-    input_resize = imresize(input_resize, [h, w]);    
-    %disp(input_resize); 
-    
-    dist = 0; 
+    input_resize = imresize(img, [h, w]); 
+    input_resize (input_resize < 0.9) = 0;
+    input_resize (input_resize >= 0.9) = 1;  
+    count = 0; 
     for x = 1:h
         for y = 1:w
-            dist = dist + abs(target_img(x, y) - input_resize(x, y)); 
-        end 
-    end 
-    scores(i, 1) = dist/(h*w); 
-    X = [filename, ' score is ', num2str(scores(i, 1))]; 
-    %disp(X); 
+            if target_img(x, y) == input_resize(x, y)
+               count = count+1; 
+            end
+        end
+    end
+    scores(i, 1) = count/(h*w); 
+
 end 
-    [M, I] = min(scores); 
-    %disp(M); disp(I); 
-    %disp(scores); 
-    X = [imagefiles(I).name, 'score is ', num2str(M)]; 
-    disp(X); 
+    [M, I] = max(scores); 
+    %disp(scores);
+    if I == 11 && img_h > img_w*3
+        match = -1;
+        disp('end of trunk');
+        graph = 1;
+    else
+        disp(['The closest match is image number ', num2str(I), ' with score ', num2str(M)]); 
+        match = I;
+        corr = im2double(rgb2gray(imread([folder, num2str(I), '.jpeg'])));
+        %figure (1), imshow([img, imresize(corr, [img_h, img_w])]);
+        graph = imresize(corr, [img_h, img_w]);
+    end
+    
 end
 
--- run_me.m
+++ run_me.m
@@ -11,12 +11,24 @@ file_path = [input_folder input_file_name];
 boundaries = segmentImage(cleaned_image, lines, display_intermediate_result);
 
 % Musical notation classification
+width = ceil(sqrt(segment_index));
+figure
 for segment_index = 1 : size(boundaries, 1)
     min_x = boundaries(segment_index, 1);
     max_x = boundaries(segment_index, 2);
     min_y = boundaries(segment_index, 3);
     max_y = boundaries(segment_index, 4);
     notation_segment = cleaned_image(min_y : max_y, min_x : max_x);
-
+    
+    [match, graph] = calculateDistance(notation_segment, 'simple');
+    if match ~= -1
+        if mod(segment_index, 25) == 0
+            figure
+        end
+        %display(mod(segment_index, 25));
+        subplot(5, 5, mod(segment_index, 25)+1);
+        imshow([notation_segment, graph]);
+    end
+    
     % notation_type = functionName(notation_segment);
 end
